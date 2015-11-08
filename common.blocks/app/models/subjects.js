af7f7dcb63b306da5ac0ce378e35c93ab3358986
Pavel Kucherov <php_prog@mail.ru>
2015-11-08 16:56:46 +0300

    Добавил таблицу с тестами

--- common.blocks/app/models/subjects.js
+++ common.blocks/app/models/subjects.js
@@ -8,38 +8,76 @@ module.exports = function (orm, db) {
         },
         name: {
             type: 'text',
-            unique: true,
             size: 255,
             defaultValue: ''
         }
 
-    });
+    }),
+        b = db.define('brain-tests', {
 
-    db.sync(function(err) {
+            id: {
+                type: 'serial',
+                key: true
+            },
+
+            class: {
+                type: 'integer',
+                size: 2,
+                index: true
+            },
+
+            name: {
+                type: 'text',
+                size: 255,
+                defaultValue: ''
+            },
+
+            descr: {
+                type: 'text',
+                defaultValue: ''
+            },
+
+            question: {
+                type: 'text',
+                defaultValue: ''
+            },
+
+            answers: {
+                type: 'text',
+                defaultValue: ''
+            },
+
+            rightanswernum: {
+                type: 'integer',
+                size: 2
+            }
+        });
+
+    b.hasOne('subj', s, { autoFetch: true, reverse:  'test' });
+    
+    db.sync(function (err) {
         if (err) throw err;
 
         s.create([
-            { name: 'Английский язык' },
-            { name: 'Биология' },
-            { name: 'География' },
-            { name: 'ИЗО' },
-            { name: 'Информатика' },
-            { name: 'История' },
-            { name: 'Литература' },
-            { name: 'Математика' },
-            { name: 'Музыка' },
-            { name: 'Немецкий язык' },
-            { name: 'ОБЖ' },
-            { name: 'Обществознание' },
-            { name: 'Окружающий мир' },
-            { name: 'ОРКСЭ' },
-            { name: 'Русский язык' },
-            { name: 'Технология' },
-            { name: 'Физика' },
-            { name: 'Физкультура' },
-            { name: 'Химия' }
-        ], function(err) {});
-
+            {name: 'Английский язык'},
+            {name: 'Биология'},
+            {name: 'География'},
+            {name: 'ИЗО'},
+            {name: 'Информатика'},
+            {name: 'История'},
+            {name: 'Литература'},
+            {name: 'Математика'},
+            {name: 'Музыка'},
+            {name: 'Немецкий язык'},
+            {name: 'ОБЖ'},
+            {name: 'Обществознание'},
+            {name: 'Окружающий мир'},
+            {name: 'ОРКСЭ'},
+            {name: 'Русский язык'},
+            {name: 'Технология'},
+            {name: 'Физика'},
+            {name: 'Физкультура'},
+            {name: 'Химия'}
+        ], function (err) {});
     });
-
 };
