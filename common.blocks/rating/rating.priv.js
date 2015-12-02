4f7752ff6788e4fce4b94523b869f38a78b01226
Pavel Kucherov <pskucherov@yandex.ru>
2015-12-03 00:32:53 +0300

    Бейджи в рейтинге

--- common.blocks/rating/rating.priv.js
+++ common.blocks/rating/rating.priv.js
@@ -8,21 +8,51 @@ BEMPRIV.decl('rating', {
     },
 
     getContent: function() {
+        var p = 'data:image/png;base64,';
         return [
             {
                 elem: 'user',
                 attrs: {
-                    style: 'width: 100px; height: 100px; border-radius: 100%; background-color: red; margin: 15px 0;'
+                    style: 'width: 70px; height: 70px; border-radius: 100%; background-color: red; margin: 15px 0;'
                 },
                 content: {
                     block: 'rating',
                     elem: 'stats',
-                    content: '1е место'
+                    content: {
+                        block: 'image',
+                        attrs: { style: 'position: absolute; bottom: -15px; right: 0;' },
+                        url: p + 'borschik:include:./first-place_size_sb.png'
+                    }
                 }
             },
             {
+                elem: 'user',
                 attrs: {
-                    style: 'width: 100px; height: 100px; border-radius: 100%; background-color: red; margin: 15px 0;'
+                    style: 'width: 70px; height: 70px; border-radius: 100%; background-color: red; margin: 15px 0;'
+                },
+                content: {
+                    block: 'rating',
+                    elem: 'stats',
+                    content: {
+                        block: 'image',
+                        attrs: { style: 'position: absolute; bottom: -15px; right: 0;' },
+                        url: p + 'borschik:include:./second-place_size_sb.png'
+                    }
+                }
+            },
+            {
+                elem: 'user',
+                attrs: {
+                    style: 'width: 70px; height: 70px; border-radius: 100%; background-color: red; margin: 15px 0;'
+                },
+                content: {
+                    block: 'rating',
+                    elem: 'stats',
+                    content: {
+                        block: 'image',
+                        attrs: { style: 'position: absolute; bottom: -15px; right: 0;' },
+                        url: p + 'borschik:include:./third-place_size_sb.png'
+                    }
                 }
             },
             {
@@ -32,8 +62,14 @@ BEMPRIV.decl('rating', {
                 content: '...'
             },
             {
+                elem: 'user',
                 attrs: {
-                    style: 'width: 100px; height: 100px; border-radius: 100%; background-color: red; margin: -40px 0;'
+                    style: 'width: 70px; height: 70px; border-radius: 100%; background-color: red; margin: -40px 0;'
+                },
+                content: {
+                    block: 'rating',
+                    elem: 'stats',
+                    content: ''
                 }
             }
         ];
