9549944b7a7938b499bb4e82f2936cb36a543cbb
Pavel Kucherov <pskucherov@yandex.ru>
2016-02-23 20:02:01 +0300

    Сохранение времени изучения стихотворения

--- common.blocks/app/controllers/SpeakerLearnPoem.js
+++ common.blocks/app/controllers/SpeakerLearnPoem.js
@@ -70,6 +70,37 @@ SpeakerLearnPoem.getDataOfProgressOrCreate = function(slpModel, poemId, act, use
     return deferred.promise();
 };
 
+/**
+ * Зафиксировать прогресс в изучении стихотворения, если этого ещё не сделано и вернуть данные
+ *
+ * @param slpModel
+ * @param poemId
+ * @param userId
+ * @returns {*}
+ */
+SpeakerLearnPoem.saveProgress = function(slpModel, poemId, act, userId) {
+    var deferred = vow.defer();
+
+    slpModel.find({ userId: userId, poem_id: poemId }).limit(1).run(function (err, progress) {
+        if (err) throw err;
+
+        if (_.isEmpty(progress)) {
+            deferred.resolve([]);
+        } else {
+            if (!progress[0].deltaTime) {
+                progress[0].deltaTime = progress[0].modified_at - progress[0].created_at;
+                progress[0].save(function (err) {
+                    if (err) throw err;
+                    deferred.resolve(progress[0]);
+                });
+            } else {
+                deferred.resolve(progress[0]);
+            }
+        }
+    });
+
+    return deferred.promise();
+};
 
 /**
  * Получить данные о прогрессе
