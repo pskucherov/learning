4437826482867a43c4ad5f0575e430192272ccb7
Pavel Kucherov <pskucherov@yandex.ru>
2016-06-23 23:52:28 +0300

    Вынес bemjson в bemhtml

--- common.blocks/s-consultor/s-consultor.priv.js
+++ common.blocks/s-consultor/s-consultor.priv.js
@@ -58,27 +58,10 @@ BEMPRIV.decl('s-consultor', {
                 mods: { theme : 'islands',
                     add: 's-consultor-question' },
                 mix: { block: 's-consultor', elem: 'modal' },
-                content: [
-                    {
-                        block: 's-consultor',
-                        elem: 'add-question',
-                        content: {
-                            block: 'textarea',
-                            mods: {
-                                theme: 'islands',
-                                size: 'm'
-                            },
-                            mix: { block: 's-consultor', elem: 'textarea' },
-                            placeholder: 'Введите свой вопрос'
-                        }
-                    },
-                    {
-                        block: 'button',
-                        mods: { theme: 'islands', size: 'l' },
-                        mix: { block: 's-consultor', elem: 'send-question' },
-                        text: 'Отправить на модерацию'
-                    }
-                ]
+                content: {
+                    block: 's-consultor',
+                    elem: 'popup-add-question'
+                }
             }
         ]);
 
