cc4ca3d8561302fdd957318102d84ff687e4534c
Pavel Kucherov <pskucherov@yandex.ru>
2016-07-16 23:48:14 +0300

    Вынес загрузку библиотеки в отдельный метод

--- common.blocks/s-math-tasks/s-math-tasks.priv.js
+++ common.blocks/s-math-tasks/s-math-tasks.priv.js
@@ -1,16 +1,23 @@
 BEMPRIV.decl('s-math-tasks', {
     init: function() {
-
         this.js(true);
 
         this.content([
-            'Hello, Math!',
-            {
-                block: 'page',
-                elem: 'js',
-                url: 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'
-            }
+            this._getMathLib()
         ]);
+    },
 
+    /**
+     * Добавить библиотеку для работы с формулами
+     *
+     * @returns {{block: string, elem: string, url: string}}
+     * @private
+     */
+    _getMathLib: function() {
+        return {
+            block: 'page',
+            elem: 'js',
+            url: 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'
+        };
     }
 });
